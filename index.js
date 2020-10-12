8bccde6bea03c84a428d865354fef11986b37ff8
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-12 15:39:21 +0100

    Write about valid frontmatter

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-const { Node, type, visitor } = require("melody-types");
 // const { parsers } = require("prettier/parser-yaml");
-const { Parser: MelodyParser } = require("melody-parser");
 const {
   doc: {
     builders: { concat, hardline },
   },
 } = require("prettier");
 
-class FrontMatter extends Node {
-  /**
-   * @param {string} yaml
-   */
-  constructor(yaml) {
-    super();
-    this.value = yaml;
-  }
-}
-
-type(FrontMatter, "FrontMatter");
-visitor(FrontMatter, "value");
-
-const FrontMatterParser = {
-  name: "frontmatter",
-  /**
-   *
-   * @param {MelodyParser} parser
-   * @param {*} token
-   */
-  parse: function parseFrontMatter(parser, token) {
-    console.log(parser.tokens);
-    // Discard the delimiters.
-    // If possible, parse YAML with its own parser
-    console.log(JSON.stringify(token), "\n");
-  },
-};
+// Twig doesn't support frontmatter, so use only deal with it in the printer!
 
 /*
- * YAML frontmatter must follow this format:
+ * I didn't find any specification (or the like) for YAML frontmatter
+ * so I'm making the following assumptions based on what I observed
+ * Eleventy (https://www.11ty.dev/) and Nunjucks (https://mozilla.github.io/nunjucks/)
+ * consider as "valid" YAML frontmatter.
  *
- *    ---
- *    var1: ...
- *    var2: ...
- *    ---
+ * "Valid" YAML frontmatter should look something like:
+ *     ---
+ *     hello: world
+ *     ---
  *
- * The first line must have three hyphens and a newline
- * The last line must have three hyphens as well. You can add a newline to make things Prettier
- * Between the delimiters in regular YAML syntax
+ * The restrictions are as follows:
+ * - It must begin a file (i.e. nothing, not even whitespace, should come before it).
+ * - The first line must contain only the delimiter (---) and a newline.
+ *   (Optionally, there may be whitespace between the two).
+ * - The same restriction that on the first line applies to the last,
+ *   except that the newline at the end is not mandatory.
+ * - Between the first and last line must be valid YAML syntax.
+ * - If the YAML content of the frontmatter is empty,
+ *   then the closing delimeter may immediately follow the opening one
+ *   provided there is no non-newline whitesphace after the opening one.
+ *   In other words, this is "valid" frontmatter:
+ *     ------
+ *   and so is:
+ *     ---
+ *     ---
+ *   but this is not:
+ *     --- ---
  */
 
 function printFrontMatter(node, path, print, options) {
