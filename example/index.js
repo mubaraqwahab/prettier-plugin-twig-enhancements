599c66e54d2e03ab8faca99f8a43c96c407f38a0
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-25 11:06:34 +0100

    Rename test dir to example

---
+++ example/index.js
@@ -0,0 +1,15 @@
+// Ideally, you should set up some test suites with Jest or something.
+
+const fs = require("fs");
+const prettier = require("prettier");
+
+const code = fs.readFileSync("example/index.njk").toString();
+
+const formatted = prettier.format(code, {
+  parser: "melody",
+  plugins: ["."],
+  twigMelodyPlugins: ["."],
+  twigFollowOfficialCodingStandards: false,
+});
+
+console.log(formatted);
