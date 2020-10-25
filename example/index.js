e0093d8bde673c459ccec50ea53d419f714f7d7e
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-25 15:50:38 +0100

    Add cmd arg comment

--- example/index.js
+++ example/index.js
@@ -3,6 +3,8 @@ const prettier = require("prettier");
 
 const code = fs.readFileSync("example/index.njk").toString();
 
+// Pass any command line param to enable the enhancement plugin
+// e.g. node example/index.js p
 const withPlugin = !!process.argv[2];
 
 const formatted = prettier.format(code, {
