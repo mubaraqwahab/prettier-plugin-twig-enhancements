2d48bf9c0d622d0d4984c46ac28ecf3715137cb9
Mubaraq Wahab <mub.wahab26@gmail.com>
2020-10-25 10:59:40 +0100

    Copy test suite from TOML tutorial repo

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!<rootDir>/node_modules/",
+    "!src/prettier-comments/**/*.js",
+  ],
+  setupFiles: ["<rootDir>/tests_config/run_spec.js"],
+  snapshotSerializers: ["<rootDir>/tests_config/raw-serializer.js"],
+  testEnvironment: "node",
+  testRegex: "jsfmt\\.spec\\.js$|__tests__/.*\\.js$",
+  transform: {},
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+};
